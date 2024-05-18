c584415e3622c7452255e178b7d6b8002d162cec
haz10em <haz10em@gmail.com>
2024-05-18 16:35:08 +0300

    this is the template for admin INSHA Allah I will do system school menagment

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./core/dashboard/dashboard/dashboard.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, DashboardComponent]
+})
+export class AppComponent {
+  title = 'MySchool';
+}
