5030e5dd7e77f4f8fc73d7538ba72f968bf40163
haz10em <haz10em@gmail.com>
2024-05-20 07:18:32 +0300

    I made good structure for system school and disgin the admin and add firebase ,auth and disgin the login and more ..

---
+++ src/app/components/admin/parents/all-parents/all-parents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-parents',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-parents.component.html',
+  styleUrl: './all-parents.component.scss'
+})
+export class AllParentsComponent {
+
+}
---
+++ src/app/components/admin/shard/shard.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+// import { TranslateModule } from '@ngx-translate/core';
+
+// const components=[];
+
+const modules=[
+  FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    // TranslateModule,
+    ReactiveFormsModule,
+    CommonModule
+]
+
+@NgModule({
+  // declarations:components,
+  imports: modules,
+  exports:[
+    ...modules
+  //  ...components,...modules
+  ]
+})
+export class ShardModule { }
