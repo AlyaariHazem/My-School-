33806da4e8f3b2b1e4167759ebdf3ead9057fdbf
haz10em <haz10em@gmail.com>
2024-05-29 23:13:24 +0300

    I added folder called 'sittings' in this folder I created division,grades in the stage-grades to display UI mange the stage and grades also I add components for manage years and I chose font from bootstrap

---
+++ src/app/components/admin/sittings/stages-grades/division/division.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-division',
+  templateUrl: './division.component.html',
+  styleUrl: './division.component.scss'
+})
+export class DivisionComponent {
+
+}
